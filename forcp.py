c57cc1ca01358ebceb0a09f55c465bcf87e583e7
zou <1274636255@qq.com>
2019-11-17 19:36:21 +0800

    use w and f to compare

---
+++ forcp.py
@@ -0,0 +1,9 @@
+for n in range(7):
+    a = int(input("Please enter a number: "))
+    if a<10:
+        print('a<10')
+    elif a==10:
+        print('a=10')
+    else:
+        print('a>10')
+
---
+++ whilecp.py
@@ -0,0 +1,11 @@
+n=0
+while n<7:
+    n+=1
+    a=int(input('please enter a number:'))
+    if a==10:
+        print('a=10')
+    elif a<10:
+        print('a<10')
+    else:
+        print('a>10')
+
